14a350665615c6a4da828f84f0864836ccdbe39a
Rujul Jain <rsj4000@gmail.com>
2022-03-30 11:15:05 -0500

    Added basic assets and created scrolling notes and note hit bar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeatScroller theBS;
+
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+    }
+
+    public void noteHit()
+    {
+        Debug.Log("Hit on Time");
+    }
+
+    public void noteMissed()
+    {
+        Debug.Log("Missed Note");
+    }
+}
---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPressed))
+        {
+            if(canBePressed)
+            {
+                gameObject.SetActive(false);
+
+                GameManager.instance.noteHit();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator" && gameObject.activeSelf)
+        {
+            canBePressed = false;
+
+            GameManager.instance.noteMissed();
+        }
+    }
+}
